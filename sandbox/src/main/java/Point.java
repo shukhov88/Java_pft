33b27d91536da1f455aa7aa468e492c106ba4c6e
Vladimir Shukhov (work) <shukhov88@gmail.com>
2017-03-15 13:01:18 +0300

    uploading final Task2 solution

---
+++ sandbox/src/main/java/Point.java
@@ -0,0 +1,16 @@
+
+public class Point {
+
+    public double x;
+    public double y;
+
+    public double distance(Point p) {
+        double dist = Math.sqrt((this.x - p.x)*(this.x - p.x) + (this.y - p.y)*(this.y - p.y));
+        return dist;
+    }
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ sandbox/src/main/java/Runner.java
@@ -0,0 +1,19 @@
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+        Point p1 = new Point(2, 2);
+        Point p2 = new Point(-2, -2);
+        double distMethod = p1.distance(p2);
+        System.out.println("Distance between points = " + distMethod + " (calculated in method inside object)");
+
+        double distFunction = distance(p1, p2);
+        System.out.println("Distance between points = " + distFunction + " (calculated in static function");
+    }
+
+    public static double distance(Point p1, Point p2) {
+        double dist = Math.sqrt((p1.x - p2.x)*(p1.x - p2.x) + (p1.y - p2.y)*(p1.y - p2.y));
+        return dist;
+    }
+}
