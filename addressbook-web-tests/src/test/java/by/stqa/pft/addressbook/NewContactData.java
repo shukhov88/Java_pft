09fe96f012e80404f7d9c430e1cba15121173a81
Vladimir Shukhov <shukhov88@gmail.com>
2017-03-17 00:38:58 +0300

    uploading Task 4 solution

---
+++ addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/NewContactData.java
@@ -0,0 +1,67 @@
+package by.stqa.pft.addressbook;
+
+public class NewContactData {
+    private final String firstName;
+    private final String middleName;
+    private final String lastName;
+    private final String nickName;
+    private final String title;
+    private final String company;
+    private final String address;
+    private final String mobilePhone;
+    private final String workPhone;
+    private final String email;
+
+    public NewContactData(String firstName, String middleName, String lastName, String nickName, String title, String company, String address, String mobilePhone, String workPhone, String email) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.nickName = nickName;
+        this.title = title;
+        this.company = company;
+        this.address = address;
+        this.mobilePhone = mobilePhone;
+        this.workPhone = workPhone;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public String getWorkPhone() {
+        return workPhone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
