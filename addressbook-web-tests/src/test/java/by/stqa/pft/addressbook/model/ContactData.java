4ba544796e94308278706f5838cdf0b2ac4e8ed9
Vladimir Shukhov <shukhovvg@oxagile.com>
2017-05-17 19:21:13 +0300

    Minor fix of Task17 solution

--- addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/model/ContactData.java
@@ -218,12 +218,8 @@ public class ContactData {
         if (firstName != null ? !firstName.equals(that.firstName) : that.firstName != null) return false;
         if (lastName != null ? !lastName.equals(that.lastName) : that.lastName != null) return false;
         if (address != null ? !address.equals(that.address) : that.address != null) return false;
-        if (homePhone != null ? !homePhone.equals(that.homePhone) : that.homePhone != null) return false;
         if (mobilePhone != null ? !mobilePhone.equals(that.mobilePhone) : that.mobilePhone != null) return false;
-        if (workPhone != null ? !workPhone.equals(that.workPhone) : that.workPhone != null) return false;
-        if (email != null ? !email.equals(that.email) : that.email != null) return false;
-        if (email2 != null ? !email2.equals(that.email2) : that.email2 != null) return false;
-        return email3 != null ? email3.equals(that.email3) : that.email3 == null;
+        return email != null ? email.equals(that.email) : that.email == null;
     }
 
     @Override
@@ -232,12 +228,8 @@ public class ContactData {
         result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
         result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
         result = 31 * result + (address != null ? address.hashCode() : 0);
-        result = 31 * result + (homePhone != null ? homePhone.hashCode() : 0);
         result = 31 * result + (mobilePhone != null ? mobilePhone.hashCode() : 0);
-        result = 31 * result + (workPhone != null ? workPhone.hashCode() : 0);
         result = 31 * result + (email != null ? email.hashCode() : 0);
-        result = 31 * result + (email2 != null ? email2.hashCode() : 0);
-        result = 31 * result + (email3 != null ? email3.hashCode() : 0);
         return result;
     }
 }
